619124e674d6d975d30f82ca18e44aca8de75bc1
AlphaLy <lyalpha2401@gmail.com>
2019-04-08 12:05:15 -0500

    Trivia Game: 5th project built with jQuery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,98 +1,97 @@
 var timer;
 
 $("#start").on("click",function(){
-	game.start();
+	gameplay.start();
 })
 
 var questions = [{
 	question:"what is the capital of Texas?",
-	answers:["Dallas", "Austin", "San Antonio"],
+	response:["Dallas", "Austin", "San Antonio"],
 	correctanswer: "Austin",
 },{
 	question:"what was John Kennedy know by?",
-	answers:["John", "Johnny", "Jack"],
+	response:["John", "Johnny", "Jack"],
 	correctanswer: "Jack",
 },{
-	question:"what is the capital of Texas?",
-	answers:["Dallas", "Austin", "San Antonio"],
-	correctanswer: "Austin",
+	question:"what is the independence day of Mexico?",
+	response:["October 9th", "May 5th", "September 16th"],
+	correctanswer: "September 16th",
 },{
 	question:"where was Obama born?",
-	answers:["Kenya", "Hawai", "Chicago"],
+	response:["Kenya", "Hawai", "Chicago"],
 	correctanswer: "Hawai",
 },{
-	question:"what is the capital of Texas?",
-	answers:["Dallas", "Austin", "San Antonio"],
-	correctanswer: "Austin",
+	question:"In what year there was no World Series due to a players strike.?",
+	response:["1994", "1904", "1983"],
+	correctanswer: "1994",
 }]
 
 
-var game = {
+var gameplay = {
 	correct: 0,
 	incorrect: 0,
 	counter: 15,
-	countdown: function(){
-		game.counter--;
-		$('#counter').html(game.counter);
-		if(game.counter<=0){
-			console.log("time is up");
-			game.done();
+	clock: function(){
+		gameplay.counter--;
+		$('#counter').html(gameplay.counter);
+		if(gameplay.counter<=0){
+			gameplay.done();
 		}
 	},
 
 	start: function(){
 		var timer;
-		timer = setInterval(game.countdown,1000);
+		timer = setInterval(gameplay.clock,1000);
 		$("#startcontainer").prepend('<h2>Time Remaining: <span id="counter">15</span> Seconds</h2>');
 		for(var i =0; i<questions.length; i++){
 		$("#startcontainer").append('<h2>'+questions[i].question+'</h2>');
-		for(var j=0; j<questions[i].answers.length;j++){
+		for(var j=0; j<questions[i].response.length;j++){
 
-			$("#startcontainer").append("<input type='radio' name='question-"+i+"'value='"+questions[i].answers[j]+"'>"
-				+questions[i].answers[j])
+			$("#startcontainer").append("<input type='radio' name='question-"+i+"'value='"+questions[i].response[j]+"'>"
+				+questions[i].response[j])
 				$("#start").remove()
 		}
 	}
   },
 
   done: function(){
-  	$.each($('input[name="question-0]":checked'),function(){
-  		if(($this).val()==questions[0].correctanswer){
-  			game.correct++;
+  	$.each($("input[name='question-0']:checked"),function(){
+  		if($(this).val()==questions[0].correctanswer){
+  			gameplay.correct++;
   		} else{
-  			game.incorrect++;
+  			gameplay.incorrect++;
   		}
   	});
 
-  	$.each($('input[name="question-1]":checked'),function(){
-  		if(($this).val()==questions[1].correctanswer){
-  			game.correct++;
+  	$.each($("input[name='question-1']:checked"),function(){
+  		if($(this).val()==questions[1].correctanswer){
+  			gameplay.correct++;
   		} else{
-  			game.incorrect++;
+  			gameplay.incorrect++;
   		}
   	});
 
-  	$.each($('input[name="question-2]":checked'),function(){
-  		if(($this).val()==questions[2].correctanswer){
-  			game.correct++;
+  	$.each($("input[name='question-2']:checked"),function(){
+  		if($(this).val()==questions[2].correctanswer){
+  			gameplay.correct++;
   		} else{
-  			game.incorrect++;
+  			gameplay.incorrect++;
   		}
   	});
 
-  	$.each($('input[name="question-3]":checked'),function(){
-  		if(($this).val()==questions[3].correctanswer){
-  			game.correct++;
+  	$.each($("input[name='question-3']:checked"),function(){
+  		if($(this).val()==questions[3].correctanswer){
+  			gameplay.correct++;
   		} else{
-  			game.incorrect++;
+  			gameplay.incorrect++;
   		}
   	});
 
-  	$.each($('input[name="question-4]":checked'),function(){
-  		if(($this).val()==questions[4].correctanswer){
-  			game.correct++;
+  	$.each($("input[name='question-4']:checked"),function(){
+  		if($(this).val()==questions[4].correctanswer){
+  			gameplay.correct++;
   		} else{
-  			game.incorrect++;
+  			gameplay.incorrect++;
   		}
   	});
 
@@ -105,7 +104,8 @@ var game = {
   		$('#startcontainer h2').remove();
   		$('#startcontainer').html("<h2>Game Over!</h2>");
   		$('#startcontainer').append("<h3>Correct Answers: "+this.correct+"</h3>");
-  		$('#startcontainer').append("<h3>inorrect Answers: "+this.incorrect+"</h3>");
+  		$('#startcontainer').append("<h3>Inorrect Answers: "+this.incorrect+"</h3>");
+  		$('#startcontainer').append("<h3>Unanswered: "+(questions.length-(this.incorrect+this.correct))+"</h3>");
 
 
   	}
